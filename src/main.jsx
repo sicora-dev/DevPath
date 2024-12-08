328aa1f0eca747f82b75568dd924806c119a715b
sicora-dev <sirocornejoraez@gmail.com>
2024-12-08 14:08:03 +0100

    Fix import path for SpeedInsights component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 import ContextProvider from './context/Context.jsx'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <ContextProvider>
